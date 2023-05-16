5454a4e87399078750caf53bdc64dfa05ab02d78
= <gabrielcardoso95@gmail.com>
2023-05-16 02:22:52 -0300

    Codigo sincronizado com os videos

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy import Column, Integer, String
 from geoalchemy2 import Geometry
-
 Base = declarative_base()
 
 class Edificacao(Base):
@@ -10,4 +9,4 @@ class Edificacao(Base):
 
     id = Column('id', Integer, primary_key=True)
     nome = Column('nome', String(length=100))
-    geom = Column('geom',Geometry(geometry_type='POLYGON', srid=4674, from_text='ST_GeomFromEWKT', name='geometry'),nullable=True)
+    geom = Column('geom', Geometry(geometry_type='POLYGON', srid=4674, from_text='ST_GeomFromEWKT', name='geometry'), nullable=True)
--- server.py
+++ server.py
@@ -1,81 +1,58 @@
-import json
-
 from sanic import Sanic, response
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from sqlalchemy import select
-from shapely.geometry import shape
+from sqlalchemy import create_engine, select
 from models import Edificacao
 from geoalchemy2.functions import ST_AsGeoJSON
-
-engine = create_engine("postgresql://postgres:postgres@localhost/postgis_33_sample", echo=True)
+import json
+from shapely.geometry import shape
+from sqlalchemy.orm import Session
 app = Sanic(__name__)
 
-@app.route("/edificacoes", methods=["GET"])
-async def list_edificacoes(request):
-    features = []
+engine = create_engine("postgresql://postgres:postgres@localhost:5432/postgis_33_sample", echo=True)
 
-    stmt = select(Edificacao.id, Edificacao.nome, ST_AsGeoJSON(Edificacao.geom)).order_by(Edificacao.id)
+@app.route("edificacao", methods=["GET"])
+async def listar_edificacoes(request):
+    feature_collection = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+    query = select(Edificacao.id, Edificacao.nome, ST_AsGeoJSON(Edificacao.geom)).order_by(Edificacao.id)
     with engine.connect() as conn:
-        rows = conn.execute(stmt)
+        rows = conn.execute(query)
         for id, nome, geometry in rows:
             feature = {
-                "id": f"{id}",
+                "id": id,
                 "type": "Feature",
                 "properties": {
-                    "nome": f"{nome}"
+                    "nome": nome
                 },
                 "geometry": json.loads(geometry)
             }
-            features.append(feature)
-
-    feature_collection = {
-        "type": "FeatureCollection",
-        "features": features
-    }
+            feature_collection["features"].append(feature)
 
     if 'text/html' in request.headers.get("accept").split(","):
         with open('map.html') as map_file:
             map_file_content = map_file.read()
             map_file_content = map_file_content.replace('const geojsonObject = {}', f'const geojsonObject = {json.dumps(feature_collection)}')
         return response.html(map_file_content)
-    return response.json(feature_collection)
+    return response.json(feature_collection, status=200)
 
-@app.route("/edificacoes", methods=["POST"])
-async def create_edificacoes(request):
-
+@app.route("edificacao", methods=["POST"])
+async def criar_edificacoes(request):
     geom = shape(request.json['geometry'])
-    edificacao = Edificacao(nome=request.json['properties']['nome'], geom=f"SRID=4674;{geom}")
+    geom_str = f"SRID=4674;{geom}"
 
+    edificacao = Edificacao(
+        nome=request.json['properties']['nome'],
+        geom=geom_str
+    )
     with Session(engine) as session:
         session.add(edificacao)
         session.commit()
 
-    return response.json({"Message": "Created!"}, status=201)
+    return response.json({"Feito": "Edificacao salva com sucesso"}, status=201)
 
-@app.route("/edificacoes/<id:int>", methods=["GET"])
-async def get_edificacao(request, id):
-    stmt = select(Edificacao.id, Edificacao.nome, ST_AsGeoJSON(Edificacao.geom)).where(Edificacao.id == id)
-    with Session(engine) as session:
-        resp = session.execute(stmt).first()
-        if resp is None:
-            return response.json({'Resultado': 'Recurso não encontrado'}, status=404)
-
-        id, nome, geometry = resp
-
-        feature = {
-            "id": f"{id}",
-            "type": "Feature",
-            "properties": {
-                "nome": f"{nome}"
-            },
-            "geometry": json.loads(geometry)
-        }
-
-    return response.json(feature, status=200)
-
-@app.route("/edificacoes/<id:int>", methods=["PUT"])
-async def update_edificacao(request, id):
+@app.route("edificacao/<id:int>", methods=["PUT"])
+async def alterar_edificacao(request, id):
 
     with Session(engine) as session:
         edificacao = session.get(Edificacao, id)
@@ -84,9 +61,8 @@ async def update_edificacao(request, id):
         session.commit()
     return response.json(body=None, status=204)
 
-
-@app.route("/edificacoes/<id:int>", methods=["DELETE"])
-async def delete_edificacao(request, id):
+@app.route("edificacao/<id:int>", methods=["DELETE"])
+async def remover_edificacao(request, id):
     with Session(engine) as session:
         edicicacao = session.get(Edificacao, id)
         session.delete(edicicacao)
@@ -94,4 +70,4 @@ async def delete_edificacao(request, id):
     return response.json(body=None, status=204)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8500)
+    app.run(host="0.0.0.0", port=8050)
